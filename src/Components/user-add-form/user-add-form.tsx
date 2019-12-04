6e6796cf069ee6325f9a88d3184a2f93ad14dcf9
ChekhDanil <chekhdan@gmail.com>
2019-12-04 12:35:41 +0200

    working mutations with refetch

--- src/Components/user-add-form/user-add-form.tsx
+++ src/Components/user-add-form/user-add-form.tsx
@@ -1,35 +1,46 @@
-import React from 'react';
-import {useMutation} from '@apollo/react-hooks';
-import {toast} from "react-toastify";
+import React from "react";
+import { useMutation } from "@apollo/react-hooks";
+import { toast } from "react-toastify";
 
 import ADD_USER from "./mutations";
-import './user-add-form.css';
-import 'react-toastify/dist/ReactToastify.css';
+import GET_USERS from "../../Pages/users-page/queries";
+import "./user-add-form.css";
+import "react-toastify/dist/ReactToastify.css";
 
 const AddTodo = () => {
-    let input: HTMLInputElement | null;
-    const [addTodo] = useMutation(ADD_USER);
-    return (
-        <div>
-            <form
-                onSubmit={e => {
-                    e.preventDefault();
-                    addTodo({variables: {name: input!.value}})
-                        .then(res =>
-                            toast.success(`User ${res.data.insert_users.returning[0].name} Added`,
-                                {position: "bottom-right"}));
-                    input!.value = '';
-                }}
-            >
-                <input
-                    ref={node => {
-                        input = node;
-                    }}
-                />
-                <button type="submit" className='add_user_button'>Add User</button>
-            </form>
-        </div>
-    );
+  let input: HTMLInputElement | null;
+  const [addTodo] = useMutation(ADD_USER, {
+    refetchQueries: [
+      {
+        query: GET_USERS
+      }
+    ]
+  });
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          addTodo({ variables: { name: input!.value } }).then(res =>
+            toast.success(
+              `User ${res.data.insert_users.returning[0].name} Added`,
+              { position: "bottom-right" }
+            )
+          );
+          input!.value = "";
+        }}
+      >
+        <input
+          ref={node => {
+            input = node;
+          }}
+        />
+        <button type="submit" className="add_user_button">
+          Add User
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AddTodo;
--- src/Pages/users-page/users.tsx
+++ src/Pages/users-page/users.tsx
@@ -1,49 +1,54 @@
 import React from "react";
-import {useQuery, useMutation,useSubscription} from '@apollo/react-hooks';
+import { useQuery, useMutation, useSubscription } from "@apollo/react-hooks";
 
 import GET_USERS from "./queries";
 import UserAddForm from "../../Components/user-add-form";
-import './users.css'
+import "./users.css";
 
 import DELETE_USER from "./mutations";
 import USERS_SUBSCRIBE from "./subscriptions";
-import { faUserMinus} from '@fortawesome/free-solid-svg-icons'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faUserMinus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Users = () => {
-
-
-
-    const {error, loading, data} = useQuery(GET_USERS);
-    const [deleteUser] = useMutation(DELETE_USER);
-    const users = useSubscription(USERS_SUBSCRIBE);
-    console.log('users',users)
-    return (
-        <div className='users_page'>
-            <h1>Users</h1>
-            <UserAddForm/>
-            {
-                error
-                    ? "Error!" : loading
-                    ? "Loading..."
-                    :
-                    data.users.map(({id, name}: {
-                        id: string, name: string,
-                    }) => (
-                        <div key={id}>
-                            <div className="users_details">
-                                <h2 className='user_name'>User name:{name}
-                                    <FontAwesomeIcon className="delete_user_icon"
-                                                     onClick={() =>
-                                                         deleteUser({variables: {userName: name}})}
-                                                     icon={faUserMinus}/>
-                                </h2>
-                                <span className="user_id">User id {id}</span>
-                            </div>
-                        </div>
-                    ))
-            }
-        </div>
-    )
+  const { error, loading, data } = useQuery(GET_USERS);
+  const [deleteUser] = useMutation(DELETE_USER, {
+    refetchQueries: [
+      {
+        query: GET_USERS
+      }
+    ]
+  });
+  const users = useSubscription(USERS_SUBSCRIBE);
+  console.log("users", users);
+  return (
+    <div className="users_page">
+      <h1>Users</h1>
+      <UserAddForm />
+      {error
+        ? "Error!"
+        : loading
+        ? "Loading..."
+        : data.users.map(({ id, name }: { id: string; name: string }) => (
+            <div key={id}>
+              <div className="users_details">
+                <h2 className="user_name">
+                  User name:{name}
+                  <FontAwesomeIcon
+                    className="delete_user_icon"
+                    onClick={() =>
+                      deleteUser({
+                        variables: { userName: name }
+                      })
+                    }
+                    icon={faUserMinus}
+                  />
+                </h2>
+                <span className="user_id">User id {id}</span>
+              </div>
+            </div>
+          ))}
+    </div>
+  );
 };
 export default Users;
